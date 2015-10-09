1e1d5a2fb8b2a8bcb740aea8f99d96475daea24c
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-09 09:59:51 +0200

    [UPD] prefix versions with 8.0

--- base_import_async/__openerp__.py
+++ base_import_async/__openerp__.py
@@ -23,7 +23,7 @@
 ###############################################################################
 {
     'name': 'Asynchronous Import',
-    'version': '1.0',
+    'version': '8.0.1.0.0',
     'author': 'Akretion, ACSONE SA/NV, Odoo Community Association (OCA)',
     'license': 'AGPL-3',
     'category': 'Generic Modules',
