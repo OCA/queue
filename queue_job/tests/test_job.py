95716e981bafdbf0d6372e77492278c11e0b4c37
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2016-11-02 15:17:26 +0100

    Use short copyrights

--- queue_job/tests/test_job.py
+++ queue_job/tests/test_job.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# Copyright 2013-2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 import mock
 from datetime import datetime, timedelta
--- queue_job/tests/test_runner_channels.py
+++ queue_job/tests/test_runner_channels.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+# Copyright 2015-2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
 import doctest
 from odoo.addons.queue_job.jobrunner import channels
 
