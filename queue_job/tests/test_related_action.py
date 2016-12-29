43bb875adc421f6773a07ae6ba143014d7a0e600
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2016-12-29 23:02:15 +0100

    Fix implementation of related actions

--- queue_job/tests/test_related_action.py
+++ queue_job/tests/test_related_action.py
@@ -4,7 +4,6 @@
 
 import odoo.tests.common as common
 from odoo.addons.queue_job.job import Job
-from .common import related_actionify
 
 
 class TestRelatedAction(common.TransactionCase):
@@ -14,47 +13,9 @@ class TestRelatedAction(common.TransactionCase):
         super(TestRelatedAction, self).setUp()
         self.method = self.env['queue.job'].testing_method
 
-    def test_no_related_action(self):
-        """ Job without related action """
-        job = Job(self.method)
-        self.assertIsNone(job.related_action())
-
-    def test_return_none(self):
-        """ Job with related action returning None """
-        # default action returns None
-        with related_actionify(self.method):
-            job = Job(self.method)
-            self.assertIsNone(job.related_action())
-
     def test_return(self):
         """ Job with related action check if action returns correctly """
-        with related_actionify(self.method, action='testing_related_method'):
-            job = Job(self.method)
-            act_job, act_kwargs = job.related_action()
-            self.assertEqual(act_job, job.db_record())
-            self.assertEqual(act_kwargs, {})
-
-    def test_kwargs(self):
-        """ Job with related action check if action propagates kwargs """
-        action = 'testing_related_method'
-        with related_actionify(self.method, action=action, b=4):
-            job = Job(self.method)
-            self.assertEqual(job.related_action(), (job.db_record(), {'b': 4}))
-
-    def test_store_related_action(self):
-        """ Call the related action on the model """
-        action = 'testing_related_method'
-        with related_actionify(self.method,
-                               action=action,
-                               url='https://en.wikipedia.org/wiki/{subject}'):
-            job = Job(self.method, args=('Discworld',))
-            job.store()
-            stored_job = self.env['queue.job'].search(
-                [('uuid', '=', job.uuid)]
-            )
-            self.assertEqual(len(stored_job), 1)
-            expected = {'type': 'ir.actions.act_url',
-                        'target': 'new',
-                        'url': 'https://en.wikipedia.org/wiki/Discworld',
-                        }
-            self.assertEquals(stored_job.open_related_action(), expected)
+        job = Job(self.method)
+        act_job, act_kwargs = job.related_action()
+        self.assertEqual(act_job, job.db_record())
+        self.assertEqual(act_kwargs, {})
--- test_queue_job/models/test_models.py
+++ test_queue_job/models/test_models.py
@@ -2,9 +2,27 @@
 # Copyright 2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from odoo.addons.queue_job.exception import RetryableJobError
 from odoo import api, fields, models
-from odoo.addons.queue_job.job import job
+from odoo.addons.queue_job.job import job, related_action
+
+
+class QueueJob(models.Model):
+
+    _inherit = 'queue.job'
+
+    @api.multi
+    def testing_related__none(self, **kwargs):
+        return None
+
+    @api.multi
+    def testing_related__url(self, **kwargs):
+        assert 'url' in kwargs, "url required"
+        subject = self.args[0]
+        return {
+            'type': 'ir.actions.act_url',
+            'target': 'new',
+            'url': kwargs['url'].format(subject=subject),
+        }
 
 
 class TestQueueJob(models.Model):
@@ -13,20 +31,6 @@ class TestQueueJob(models.Model):
     _description = "Test model for queue.job"
 
     name = fields.Char()
-
-    @job
-    @api.multi
-    def normal_job(self, *args, **kwargs):
-        """ Method used for tests
-
-        Return always the arguments and keyword arguments received
-        """
-        return
-        if kwargs.get('raise_retry'):
-            raise RetryableJobError('Must be retried later')
-        if kwargs.get('return_context'):
-            return self.env.context
-        return args, kwargs
 
     @job
     def no_description(self):
@@ -44,17 +48,6 @@ class TestQueueJob(models.Model):
     def mapped(self, func):
         return super(TestQueueJob, self).mapped(func)
 
-    @api.multi
-    def testing_related_method(self, **kwargs):
-        if 'url' in kwargs:
-            subject = self.args[0]
-            return {
-                'type': 'ir.actions.act_url',
-                'target': 'new',
-                'url': kwargs['url'].format(subject=subject),
-            }
-        return self, kwargs
-
 
 class TestQueueChannel(models.Model):
 
@@ -71,4 +64,30 @@ class TestQueueChannel(models.Model):
 
     @job(default_channel='root.sub.subsub')
     def job_sub_channel(self):
+        return
+
+
+class TestRelatedAction(models.Model):
+
+    _name = 'test.related.action'
+    _description = "Test model for related actions"
+
+    @job
+    def testing_related_action__no(self):
+        return
+
+    @job
+    @related_action()  # default action returns None
+    def testing_related_action__return_none(self):
+        return
+
+    @job
+    @related_action(action='testing_related_method', b=4)
+    def testing_related_action__kwargs(self):
+        return
+
+    @job
+    @related_action(action='testing_related__url',
+                    url='https://en.wikipedia.org/wiki/{subject}')
+    def testing_related_action__store(self):
         return
