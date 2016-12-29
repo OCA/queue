7d26400729aab9b5876dc368d0ad8d84340aaf88
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2016-12-29 21:23:43 +0100

    Make tests pass
    Separate tests that need special methods in a dedicated test_queue_job
    module.
    
    Rework the registry of the jobs. The 'job' decorator is applied on
    functions before they are bound to their class, so we don't know the
    class yet. Now, the decorator marks job methods with 'delayable' and
    when a model is registered (Base._register_hook), it looks for the
    marked methods and register them.

--- queue_job/tests/__init__.py
+++ queue_job/tests/__init__.py
@@ -4,5 +4,4 @@ from . import test_job
 from . import test_related_action
 from . import test_runner_channels
 from . import test_runner_runner
-from . import test_utils
 from . import test_json_field
