ad095954a25ac3b78aff67ceb306b759820f5238
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-08-29 19:44:19 +0200

    Update pre-migration.py

--- queue_job/migrations/10.0.1.0.0/pre-migration.py
+++ queue_job/migrations/10.0.1.0.0/pre-migration.py
@@ -6,8 +6,12 @@
 def migrate(cr, version):
     if not version:
         return
-    # In order to migrate connector from v. 9.0 to v. 10.0, we need to set
-    # connector module state to 'to upgrade'
+    # In order to migrate connector from v9 to v10, we need to set
+    # connector module state to 'to upgrade'. If this is not done, all 
+    # connector.* xmlids are removed due to the module renaming done by 
+    # OpenUpgrade. In the future the approach sketched in 
+    # https://github.com/OCA/queue/pull/23#issuecomment-325706811
+    # may provide a more generic solution.
     cr.execute("""
         UPDATE ir_module_module
         SET state='to upgrade'
