20574afd7db30d81b7bf687ee60ed5404bfe0250
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2017-09-12 14:22:20 +0200

    [IMP] queue_job_subscribe: style

--- queue_job_subscribe/models/queue_job.py
+++ queue_job_subscribe/models/queue_job.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 Cédric Pigeon
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from openerp import api, models
 
 
--- queue_job_subscribe/tests/test_job_subscribe.py
+++ queue_job_subscribe/tests/test_job_subscribe.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 Cédric Pigeon
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 import openerp.tests.common as common
 
 from openerp.addons.queue_job.job import Job
@@ -11,35 +12,36 @@ class TestJobSubscribe(common.TransactionCase):
     def setUp(self):
         super(TestJobSubscribe, self).setUp()
         grp_queue_job_manager = self.ref("queue_job.group_queue_job_manager")
-        self.other_partner_a = self.env['res.partner'].create(
-            {"name": "My Company a",
-             "is_company": True,
-             "email": "test@tes.ttest",
-             })
-        self.other_user_a = self.env['res.users'].create(
-            {"partner_id": self.other_partner_a.id,
-             "login": "my_login a",
-             "name": "my user",
-             "groups_id": [(4, grp_queue_job_manager)]
-             })
-        self.other_partner_b = self.env['res.partner'].create(
-            {"name": "My Company b",
-             "is_company": True,
-             "email": "test@tes.ttest",
-             })
-        self.other_user_b = self.env['res.users'].create(
-            {"partner_id": self.other_partner_b.id,
-             "login": "my_login_b",
-             "name": "my user 1",
-             "groups_id": [(4, grp_queue_job_manager)]
-             })
+        self.other_partner_a = self.env['res.partner'].create({
+            "name": "My Company a",
+            "is_company": True,
+            "email": "test@tes.ttest",
+        })
+        self.other_user_a = self.env['res.users'].create({
+            "partner_id": self.other_partner_a.id,
+            "login": "my_login a",
+            "name": "my user",
+            "groups_id": [(4, grp_queue_job_manager)]
+        })
+        self.other_partner_b = self.env['res.partner'].create({
+            "name": "My Company b",
+            "is_company": True,
+            "email": "test@tes.ttest",
+        })
+        self.other_user_b = self.env['res.users'].create({
+            "partner_id": self.other_partner_b.id,
+            "login": "my_login_b",
+            "name": "my user 1",
+            "groups_id": [(4, grp_queue_job_manager)]
+        })
 
     def _create_failed_job(self):
         method = self.env['res.users'].mapped
         test_job = Job(method)
         test_job.store()
-        test_job_record = self.env['queue.job'].\
-            search([('uuid', '=', test_job.uuid)])
+        test_job_record = self.env['queue.job'].search([
+            ('uuid', '=', test_job.uuid)
+        ])
         test_job_record.write({'state': 'failed'})
         return test_job_record
 
@@ -54,9 +56,9 @@ class TestJobSubscribe(common.TransactionCase):
         # Test 1: All users are followers
         #################################
         stored = self._create_failed_job()
-        users = self.env['res.users'].search(
-            [('groups_id', '=', self.ref('queue_job.group_queue_job_manager'))]
-        )
+        users = self.env['res.users'].search([
+            ('groups_id', '=', self.ref('queue_job.group_queue_job_manager')),
+        ])
         self.assertEqual(len(stored.message_follower_ids), len(users))
         expected_partners = [u.partner_id for u in users]
         self.assertSetEqual(set(stored.mapped(
@@ -72,10 +74,10 @@ class TestJobSubscribe(common.TransactionCase):
         ###########################################
         self.other_user_b.write({'subscribe_job': False})
         stored = self._create_failed_job()
-        users = self.env['res.users'].search(
-            [('groups_id', '=', self.ref('queue_job.group_queue_job_manager')),
-             ('subscribe_job', '=', True)]
-        )
+        users = self.env['res.users'].search([
+            ('groups_id', '=', self.ref('queue_job.group_queue_job_manager')),
+            ('subscribe_job', '=', True),
+        ])
         self.assertEqual(len(stored.message_follower_ids), len(users))
         expected_partners = [u.partner_id for u in users]
         self.assertSetEqual(set(stored.mapped(
