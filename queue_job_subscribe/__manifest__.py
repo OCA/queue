9e1a55c657b1fa763f98585746380917a7665543
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2017-09-12 13:57:02 +0200

    [MIG] queue_job_subscribe 10.0

---
+++ queue_job_subscribe/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Cédric Pigeon
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{'name': 'Queue Job Subscribe',
+ 'version': '10.0.1.0.0',
+ 'author': 'Acsone SA/NV,'
+           'Odoo Community Association (OCA)',
+ 'website': 'https://github.com/OCA/queue_job',
+ 'summary': 'Control which users are subscribed to queue job notifications',
+ 'license': 'AGPL-3',
+ 'category': 'Generic Modules',
+ 'depends': [
+     'queue_job'
+ ],
+ 'data': [
+     'views/res_users_view.xml',
+ ],
+ 'installable': True,
+ }
