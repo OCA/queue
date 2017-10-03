b7441bc22222edb0f5fe0eefa4da37eeb094a09b
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-10-03 09:43:23 +0200

    Migrate queue_job_subscribe to v11

--- queue_job_subscribe/tests/test_job_subscribe.py
+++ queue_job_subscribe/tests/test_job_subscribe.py
@@ -2,9 +2,9 @@
 # Copyright 2016 Cédric Pigeon
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import openerp.tests.common as common
+import odoo.tests.common as common
 
-from openerp.addons.queue_job.job import Job
+from odoo.addons.queue_job.job import Job
 
 
 class TestJobSubscribe(common.TransactionCase):
