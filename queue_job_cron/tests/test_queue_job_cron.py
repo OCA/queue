0c7b49582ac1e47555ebe89ee7da14b5c10d7549
Cédric Pigeon <cedric.pigeon@acsone.eu>
2017-12-21 11:56:10 +0100

    Improve tests coverage

--- queue_job_cron/tests/test_queue_job_cron.py
+++ queue_job_cron/tests/test_queue_job_cron.py
@@ -2,6 +2,7 @@
 # Copyright 2017 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from odoo.tests.common import TransactionCase
+from odoo.exceptions import ValidationError
 
 
 class TestQueueJobCron(TransactionCase):
@@ -30,3 +31,36 @@ class TestQueueJobCron(TransactionCase):
         self.assertEqual(qjob.priority, cron.priority)
         self.assertEqual(qjob.user_id, cron.user_id)
         self.assertEqual(qjob.channel, cron.channel_id.name)
+
+    def test_queue_job_cron_onchange(self):
+        cron = self.env.ref('queue_job.ir_cron_autovacuum_queue_jobs')
+        default_channel = self.env.ref('queue_job_cron.channel_root_ir_cron')
+        self.assertFalse(cron.run_as_queue_job)
+        cron.write({'run_as_queue_job': True})
+        cron.onchange_run_as_queue_job()
+        self.assertEqual(cron.channel_id.id, default_channel.id)
+
+    def test_queue_job_cron_run(self):
+        cron = self.env.ref('queue_job.ir_cron_autovacuum_queue_jobs')
+        IrCron = self.env['ir.cron']
+
+        model_name = cron.model
+        method_name = cron.function
+        args = cron.args
+
+        model_name = 'fake.model'
+        method_name = cron.function
+        args = cron.args
+        with self.assertRaises(ValidationError):
+            IrCron._run_job_as_queue_job(model_name, method_name, args)
+
+        model_name = cron.model
+        method_name = 'fake.function'
+        args = cron.args
+        with self.assertRaises(ValidationError):
+            IrCron._run_job_as_queue_job(model_name, method_name, args)
+
+        model_name = cron.model
+        method_name = cron.function
+        args = cron.args
+        IrCron._run_job_as_queue_job(model_name, method_name, args)
