e49ca02ae918a36e7023a2fc08b669138a8acf03
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-02-13 15:56:01 +0100

    queue_job_cron: bump version

--- queue_job_cron/__manifest__.py
+++ queue_job_cron/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Scheduled Actions as Queue Jobs',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/queue/tree/10.0/queue_job_cron',
     'license': 'AGPL-3',
