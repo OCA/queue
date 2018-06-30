0f449ff5981d31474f526ab5c7f7155f51de927d
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-06-30 02:21:23 +0200

    [IMP] queue_job: Migration with read_group

--- queue_job/migrations/10.0.1.0.0/end-migration.py
+++ queue_job/migrations/10.0.1.0.0/end-migration.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Tecnativa - Pedro M. Baeza
+# Copyright 2017-2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, SUPERUSER_ID
@@ -12,13 +12,18 @@ def migrate(cr, version):
     if not version:
         return
     env = api.Environment(cr, SUPERUSER_ID, {})
-    jobs = env['queue.job'].search([])
-    for job in jobs:
-        if job.model_name not in env:
+    QueueJob = env['queue.job']
+    groups = QueueJob.read_groups(
+        [], ['model_name', 'method_name'], ['model_name', 'method_name'],
+        lazy=False,
+    )
+    for group in groups:
+        if group['model_name'] not in env:
             continue
-        model = env[job.model_name]
-        method = getattr(model, job.method_name, False)
-        if method:
-            job.channel_method_name = '<%s>.%s' % (
+        model = env[group['model_name']]
+        method = getattr(model, group['method_name'], False)
+        QueueJob.search(group['__domain']).write({
+            'channel_method_name': '<%s>.%s' % (
                 method.im_class._name, method.__name__,
-            )
+            ),
+        })
