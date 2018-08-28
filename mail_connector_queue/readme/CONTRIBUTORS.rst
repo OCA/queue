62672b53a2d34422ee595c9f5f7cf64a9335a747
François Honoré <francois.honore@acsone.eu>
2018-08-28 14:05:10 +0200

    [MIG][11.0] mail_queue_job (old name: mail_connector_queue from connector repo) [UPD] Update to integrate identity_key (from queue_job)

--- mail_connector_queue/readme/CONTRIBUTORS.rst
+++
@@ -1,1 +0,0 @@
-* André Paramés (https://acsone.eu) <github@andreparames.com>
--- mail_queue_job/__manifest__.py
+++ mail_queue_job/__manifest__.py
@@ -1,23 +1,19 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 {
-    'name': 'Mail Connector Queue',
+    'name': 'Mail Queue Job',
     'summary': """
-        Email Connector Queue""",
-    'version': '8.0.1.0.1',
+        Mail Queue Job""",
+    'version': '11.0.1.0.1',
     'license': 'AGPL-3',
     'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/connector',
+    'website': 'https://github.com/OCA/queue',
     'depends': [
-        'connector',
+        'queue_job',
         'mail',
     ],
     'data': [
         'views/mail_mail.xml',
-    ],
-    'demo': [
     ],
     'post_init_hook': 'post_init_hook',
     'uninstall_hook': 'uninstall_hook',
