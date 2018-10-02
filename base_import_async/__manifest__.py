6cd85a147398de5d07179261347c44b05e2eb914
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2018-10-02 12:08:12 +0200

    Migrate base_import_async to 12.0

--- base_import_async/__manifest__.py
+++ base_import_async/__manifest__.py
@@ -3,15 +3,22 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Asynchronous Import",
-    "summary": "Import CSV files in the background",
-    "version": "11.0.1.0.0",
-    "author": "Akretion, ACSONE SA/NV, Odoo Community Association (OCA)",
-    "license": "AGPL-3",
-    "website": "https://github.com/OCA/queue",
-    "category": "Generic Modules",
-    "depends": ["base_import", "queue_job"],
-    "data": ["views/base_import_async.xml"],
-    "qweb": ["static/src/xml/import.xml"],
-    "installable": False,
+    'name': 'Asynchronous Import',
+    'summary': 'Import CSV files in the background',
+    'version': '12.0.1.0.0',
+    'author': 'Akretion, ACSONE SA/NV, Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/OCA/queue',
+    'category': 'Generic Modules',
+    'depends': [
+        'base_import',
+        'queue_job',
+    ],
+    'data': [
+        'views/base_import_async.xml',
+    ],
+    'qweb': [
+        'static/src/xml/import.xml',
+    ],
+    'installable': True,
 }
