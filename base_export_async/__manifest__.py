ddd5e9cfa1ecbdb3c812d468d9872a30d6df039c
Arnaud Pineux <arnaud.pineux@acsone.eu>
2019-04-19 11:52:10 +0200

    [ADD] New module base_export_async to allow asynchronous export

--- base_export_async/__manifest__.py
+++ base_export_async/__manifest__.py
@@ -2,30 +2,22 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Base Export Async",
-    "summary": "Asynchronous export with job queue",
-    "version": "17.0.1.0.0",
-    "license": "AGPL-3",
-    "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/queue",
-    "depends": ["web", "queue_job"],
-    "data": [
-        # added this additional rule as when the export happens
-        # from queue job module there is no user is there,
-        # which was causing an issue when fetching res.currency
-        "security/ir.model.access.csv",
-        "security/ir_rule.xml",
-        "data/config_parameter.xml",
-        "data/cron.xml",
-        "data/mail_template.xml",
+    'name': 'Base Export Async',
+    'summary': """
+        Asynchronous export with job queue
+        """,
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
+    'website': 'https://acsone.eu/',
+    'depends': [
+        'web',
+        'queue_job'
     ],
-    "demo": [],
-    "assets": {
-        "web.assets_backend": [
-            "base_export_async/static/src/xml/base.xml",
-            "base_export_async/static/src/js/list_controller.esm.js",
-            "base_export_async/static/src/js/data_export.esm.js",
-        ],
-    },
-    "installable": True,
+    'data': [
+        'views/assets.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': ['static/src/xml/base.xml']
 }
--- base_export_async/models/delay_export.py
+++ base_export_async/models/delay_export.py
@@ -1,154 +1,85 @@
 # Copyright 2019 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import base64
+import logging
 import json
 import operator
+import base64
 
-from dateutil.relativedelta import relativedelta
+from odoo import api, fields, models, _
+from odoo.addons.queue_job.job import job
+from odoo.addons.web.controllers.main import CSVExport, ExcelExport
+from odoo.exceptions import Warning
 
-from odoo import SUPERUSER_ID, _, api, fields, models
-from odoo.exceptions import UserError
-from odoo.http import request
-
-from odoo.addons.web.controllers.export import CSVExport, ExcelExport
+_logger = logging.getLogger(__name__)
 
 
 class DelayExport(models.Model):
-    _name = "delay.export"
-    _description = "Asynchronous Export"
 
-    user_ids = fields.Many2many("res.users", string="Users", index=True)
-    model_description = fields.Char()
-    url = fields.Char()
-    expiration_date = fields.Date()
+    _name = 'delay.export'
+    _description = 'Allow to delay the export'
 
     @api.model
     def delay_export(self, data):
-        """Delay the export, called from js"""
-        params = json.loads(data.get("data"))
-        if not self.env.user.email:
-            raise UserError(_("You must set an email address to your user."))
+        params = json.loads(data.get('data'))
+        context = params.get('context', {})
+        uid = context.get('uid', False)
+        if not uid:
+            raise Warning(_("A problem occurs during the job creation. Please contact your administrator"))
+        user = self.env['res.users'].browse([uid])
+        if not user.email:
+            raise Warning(_("You must set an email address to your user."))
         self.with_delay().export(params)
 
     @api.model
-    def _get_file_content(self, params):
-        export_format = params.get("format")
+    @job
+    def export(self, params):
+        export_format = params.get('format')
+        raw_data = export_format != 'csv'
 
-        items = operator.itemgetter(
-            "model", "fields", "ids", "domain", "import_compat", "context", "user_ids"
-        )(params)
-        (model_name, fields_name, ids, domain, import_compat, context, user_ids) = items
+        model_name, fields_name, ids, domain, import_compat, context = \
+            operator.itemgetter('model', 'fields', 'ids', 'domain', 'import_compat', 'context')(params)
+        user = self.env['res.users'].browse([context.get('uid')])
+        if not user or not user.email:
+            raise Warning(_("The user doesn't have an email address."))
 
-        model = (
-            self.env[model_name]
-            .sudo()
-            .with_context(import_compat=import_compat, **context)
-        )
-        records = model.browse(ids) or model.search(
-            domain, offset=0, limit=False, order=False
-        )
+        model = self.env[model_name].with_context(import_compat=import_compat, **context)
+        records = model.browse(ids) or model.search(domain, offset=0, limit=False, order=False)
 
         if not model._is_an_ordinary_table():
-            fields_name = [field for field in fields_name if field["name"] != "id"]
+            fields_name = [field for field in fields_name if field['name'] != 'id']
 
-        field_names = [f["name"] for f in fields_name]
-        import_data = records.export_data(field_names).get("datas", [])
+        field_names = [f['name'] for f in fields_name]
+        import_data = records.export_data(field_names, raw_data).get('datas', [])
 
         if import_compat:
             columns_headers = field_names
         else:
-            columns_headers = [val["label"].strip() for val in fields_name]
+            columns_headers = [val['label'].strip() for val in fields_name]
 
-        # Patch:
-        # ensure request.env is set with a valid uid before triggering ExportXlsxWriter
-        if export_format != "csv":
-            if not hasattr(request, "env") or request.env.uid is None:
-                request.env = api.Environment(
-                    self.env.cr, SUPERUSER_ID, self.env.context
-                )
-
-        if export_format == "csv":
+        if export_format == 'csv':
             csv = CSVExport()
-            return csv.from_data(columns_headers, import_data)
+            result = csv.from_data(columns_headers, import_data)
         else:
             xls = ExcelExport()
-            return xls.from_data(columns_headers, import_data)
+            result = xls.from_data(columns_headers, import_data)
 
-    @api.model
-    def export(self, params):
-        """Delayed export of a file sent by email
+        attachment = self.env['ir.attachment'].create({
+            'name': "{}.{}".format(model_name, export_format),
+            'datas': base64.b64encode(result),
+            'datas_fname': "{}.{}".format(model_name, export_format),
+            'type': 'binary'
+        })
 
-        The ``params`` is a dict of parameters, contains:
-
-        * format: csv/excel
-        * model: model to export
-        * fields: list of fields to export, a list of dict:
-          [{"label": "", "name": ""}]
-        * ids: list of ids to export
-        * domain: domain for the export
-        * context: context for the export (language, ...)
-        * import_compat: if the export is export/import compatible (boolean)
-        * user_ids: optional list of user ids who receive the file
-        """
-        content = self._get_file_content(params)
-
-        items = operator.itemgetter("model", "context", "format", "user_ids")(params)
-        model_name, context, export_format, user_ids = items
-        users = self.env["res.users"].browse(user_ids)
-
-        export_record = self.sudo().create({"user_ids": [(6, 0, users.ids)]})
-
-        name = f"{model_name}.{export_format}"
-        attachment = self.env["ir.attachment"].create(
-            {
-                "name": name,
-                "datas": base64.b64encode(content),
-                "type": "binary",
-                "res_model": self._name,
-                "res_id": export_record.id,
-            }
-        )
-
-        url = "{}/web/content/ir.attachment/{}/datas/{}?download=true".format(
-            self.env["ir.config_parameter"].sudo().get_param("web.base.url"),
-            attachment.id,
-            attachment.name,
-        )
-
-        time_to_live = (
-            self.env["ir.config_parameter"].sudo().get_param("attachment.ttl", 7)
-        )
-        date_today = fields.Date.today()
-        expiration_date = fields.Date.to_string(
-            date_today + relativedelta(days=+int(time_to_live))
-        )
-
-        odoo_bot = self.sudo().env.ref("base.partner_root")
-        email_from = odoo_bot.email
-        model_description = self.env[model_name]._description
-        export_record.write(
-            {
-                "url": url,
-                "expiration_date": expiration_date,
-                "model_description": model_description,
-            }
-        )
-
-        self.env.ref("base_export_async.delay_export_mail_template").send_mail(
-            export_record.id,
-            email_values={
-                "email_from": email_from,
-                "reply_to": email_from,
-                "recipient_ids": [(6, 0, users.mapped("partner_id").ids)],
-            },
-        )
-
-    @api.model
-    def cron_delete(self):
-        time_to_live = (
-            self.env["ir.config_parameter"].sudo().get_param("attachment.ttl", 7)
-        )
-        date_today = fields.Date.today()
-        date_to_delete = date_today + relativedelta(days=-int(time_to_live))
-        self.search([("create_date", "<=", date_to_delete)]).unlink()
+        odoobot = self.env.ref("base.partner_root")
+        email_from = odoobot.email
+        self.env['mail.mail'].create({
+            'email_from': email_from,
+            'reply_to': email_from,
+            'email_to': user.email,
+            'subject': _("Export {} {}").format(model_name,
+                fields.Date.to_string(fields.Date.today())),
+            'body_html': _("This is an automated message please do not reply."),
+            'attachment_ids': [(4, attachment.id)],
+            'auto_delete': True,
+        })
