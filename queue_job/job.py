2b3ab779de5f6907a00b965802fb2dade15724bb
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-07-02 22:39:26 +0200

    Add wait dependencies state

--- queue_job/job.py
+++ queue_job/job.py
@@ -20,6 +20,7 @@ from .exception import (NoSuchJobError,
                         RetryableJobError)
 
 
+WAIT_DEPENDENCIES = 'wait_dependencies'
 PENDING = 'pending'
 ENQUEUED = 'enqueued'
 CANCELLED = 'cancelled'
@@ -27,7 +28,8 @@ DONE = 'done'
 STARTED = 'started'
 FAILED = 'failed'
 
-STATES = [(PENDING, 'Pending'),
+STATES = [(WAIT_DEPENDENCIES, 'Wait Dependencies'),
+          (PENDING, 'Pending'),
           (ENQUEUED, 'Enqueued'),
           (STARTED, 'Started'),
           (DONE, 'Done'),
@@ -484,6 +486,8 @@ class Job(object):
         for parent in jobs:
             parent.__reverse_depends_on_uuids.add(self.uuid)
             parent._reverse_depends_on.add(self)
+        if any(j.state != DONE for j in jobs):
+            self.state = WAIT_DEPENDENCIES
 
     def add_reverse_depends(self, jobs):
         self.__reverse_depends_on_uuids |= {j.uuid for j in jobs}
@@ -516,7 +520,19 @@ class Job(object):
                                          )
                 raise new_exc from err
             raise
+
         return self.result
+
+    # TODO call in an isolated transaction with retries (in RunJobController)
+    def enqueue_waiting(self):
+        children = self.reverse_depends_on
+        for child in children:
+            if child.state != WAIT_DEPENDENCIES:
+                continue
+            parents = child.depends_on
+            if all(parent.state == 'done' for parent in parents):
+                child.state = PENDING
+                child.store()
 
     def store(self):
         """Store the Job"""
