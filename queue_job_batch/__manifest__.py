f93a69c04935a8984db96571086ca94c206233e6
Lois Rilo <lois.rilo@eficent.com>
2019-07-12 11:52:27 +0200

    [11.0][FIX] queue_job_batch: * incorrect sintax in security rule * change missleading name of security rule

--- queue_job_batch/__manifest__.py
+++ queue_job_batch/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Job Queue Batch',
-    'version': '11.0.1.1.0',
+    'version': '11.0.1.1.1',
     'author': 'Creu Blanca,Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/queue',
     'license': 'AGPL-3',
