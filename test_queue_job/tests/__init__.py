7759c3946a94fd17fda3c82a9b750a5ab7aa772b
Naglis Jonaitis <naglis@versada.eu>
2019-08-03 18:01:01 +0300

    [ADD] Test case for "AutoVacuum Job Queue" cron job

--- test_queue_job/tests/__init__.py
+++ test_queue_job/tests/__init__.py
@@ -1,3 +1,4 @@
+from . import test_autovacuum
 from . import test_job
 from . import test_job_channels
 from . import test_related_actions
---
+++ test_queue_job/tests/test_autovacuum.py
@@ -0,0 +1,31 @@
+# Copyright 2019 Versada UAB
+# License AGPL-3 or later (https://www.gnu.org/licenses/agpl).
+
+import datetime
+
+from odoo.tests import common
+
+from odoo.addons.queue_job.job import Job
+
+
+class TestQueueJobAutovacuumCronJob(common.TransactionCase):
+
+    def setUp(self):
+        super().setUp()
+        self.queue_job = self.env['queue.job']
+        self.method = self.env['test.queue.job'].testing_method
+        self.cron_job = self.env.ref('queue_job.ir_cron_autovacuum_queue_jobs')
+
+    def test_old_jobs_are_deleted(self):
+        """
+        Old jobs are deleted by the autovacuum cron job.
+        """
+        test_job = Job(self.method)
+        test_job.set_done(result='ok')
+        test_job.date_done = datetime.datetime.now() - datetime.timedelta(
+            days=self.queue_job._removal_interval + 1)
+        test_job.store()
+
+        self.cron_job.method_direct_trigger()
+
+        self.assertFalse(test_job.db_record().exists())
