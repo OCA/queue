eeb39909d9d7e6ddaf141a133ec5fd3bc865252b
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-10-04 10:47:58 +0200

    Run pre-commit with black, isort, ...

--- queue_job_subscribe/models/queue_job.py
+++ queue_job_subscribe/models/queue_job.py
@@ -5,9 +5,9 @@ from odoo import models
 
 
 class QueueJob(models.Model):
-    _inherit = 'queue.job'
+    _inherit = "queue.job"
 
     def _subscribe_users_domain(self):
         domain = super()._subscribe_users_domain()
-        domain.append(('subscribe_job', '=', True))
+        domain.append(("subscribe_job", "=", True))
         return domain
