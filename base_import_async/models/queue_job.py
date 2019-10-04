b87d9d94836e5a67f853fa3748aa2c28f6b4462b
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-10-04 10:47:58 +0200

    Run pre-commit with black, isort, ...

--- base_import_async/models/queue_job.py
+++ base_import_async/models/queue_job.py
@@ -1,22 +1,23 @@
 # Copyright 2017 ACSONE SA/NV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, models, _
+from odoo import _, api, models
 
 
 class QueueJob(models.Model):
     """ Job status and result """
-    _inherit = 'queue.job'
+
+    _inherit = "queue.job"
 
     @api.multi
     def _related_action_attachment(self):
-        res_id = self.kwargs.get('att_id')
+        res_id = self.kwargs.get("att_id")
         action = {
-            'name': _("Attachment"),
-            'type': 'ir.actions.act_window',
-            'res_model': "ir.attachment",
-            'view_type': 'form',
-            'view_mode': 'form',
-            'res_id': res_id,
+            "name": _("Attachment"),
+            "type": "ir.actions.act_window",
+            "res_model": "ir.attachment",
+            "view_type": "form",
+            "view_mode": "form",
+            "res_id": res_id,
         }
         return action
