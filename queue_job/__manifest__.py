d84444d55365f7a6b277e5c32015a97a970692ca
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-10-31 07:42:58 +0100

    Use more permissive license AGPL3 → LGPL3
    This change has been done and accepted on the component and connector
    addons: https://github.com/OCA/connector/pull/356
    
    This is a low-level technical module, we may need to use jobs on
    enterprise features.

--- queue_job/__manifest__.py
+++ queue_job/__manifest__.py
@@ -1,11 +1,11 @@
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 
 
 {'name': 'Job Queue',
  'version': '12.0.1.0.1',
  'author': 'Camptocamp,ACSONE SA/NV,Odoo Community Association (OCA)',
  'website': 'https://github.com/OCA/queue/tree/12.0/queue_job',
- 'license': 'AGPL-3',
+ 'license': 'LGPL-3',
  'category': 'Generic Modules',
  'depends': ['mail',
              'base_sparse_field'
