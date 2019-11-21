8adc361f392b5845ffd04d8c82df2596696a1eae
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-11-21 15:40:40 +0100

    Support several recipient users in base_export_async
    The UI still send it to the current user, but it will allow
    reusing the module for other purposes: plan a regular export
    to users.
    
    Setting the recipient_ids instead of the email_to in mail.mail means
    that we can still send the email, it will stay in the failed emails,
    allowing to correct the user.

--- base_export_async/tests/test_base_export_async.py
+++ base_export_async/tests/test_base_export_async.py
@@ -2,13 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import json
-
-import odoo.tests.common as common
 from dateutil.relativedelta import relativedelta
+
+import freezegun
+
 from odoo import fields
+import odoo.tests.common as common
 
-data_csv = {
-    "data": """{"format": "csv", "model": "res.partner",
+data_csv = {'data': """{"format": "csv", "model": "res.partner",
             "fields": [{"name": "id", "label": "External ID"},
                     {"name": "display_name", "label": "Display Name"},
                     {"name": "email", "label": "Email"},
@@ -16,11 +17,11 @@ data_csv = {
             "ids": false,
             "domain": [],
             "context": {"lang": "en_US", "tz": "Europe/Brussels", "uid": 2},
-            "import_compat": false}"""
-}
+            "import_compat": false,
+            "user_ids": [2]
+            }"""}
 
-data_xls = {
-    "data": """{"format": "xls", "model": "res.partner",
+data_xls = {'data': """{"format": "xls", "model": "res.partner",
             "fields": [{"name": "id", "label": "External ID"},
                     {"name": "display_name", "label": "Display Name"},
                     {"name": "email", "label": "Email"},
@@ -28,15 +29,17 @@ data_xls = {
             "ids": false,
             "domain": [],
             "context": {"lang": "en_US", "tz": "Europe/Brussels", "uid": 2},
-            "import_compat": false}"""
-}
+            "import_compat": false,
+            "user_ids": [2]
+            }"""}
 
 
 class TestBaseExportAsync(common.TransactionCase):
+
     def setUp(self):
         super(TestBaseExportAsync, self).setUp()
-        self.delay_export_obj = self.env["delay.export"]
-        self.job_obj = self.env["queue.job"]
+        self.delay_export_obj = self.env['delay.export']
+        self.job_obj = self.env['queue.job']
 
     def test_delay_export(self):
         """ Check that the call create a new JOB"""
@@ -47,39 +50,40 @@ class TestBaseExportAsync(common.TransactionCase):
 
     def test_export_csv(self):
         """ Check that the export generate an attachment and email"""
-        params = json.loads(data_csv.get("data"))
-        mails = self.env["mail.mail"].search([])
-        attachments = self.env["ir.attachment"].search([])
+        params = json.loads(data_csv.get('data'))
+        mails = self.env['mail.mail'].search([])
+        attachments = self.env['ir.attachment'].search([])
         self.delay_export_obj.export(params)
-        new_mail = self.env["mail.mail"].search([]) - mails
-        new_attachment = self.env["ir.attachment"].search([]) - attachments
+        new_mail = self.env['mail.mail'].search([]) - mails
+        new_attachment = self.env['ir.attachment'].search([]) - attachments
         self.assertEqual(len(new_mail), 1)
-        self.assertEqual(new_attachment.datas_fname, "res.partner.csv")
+        self.assertEqual(new_attachment.datas_fname,
+                         "res.partner.csv")
 
     def test_export_xls(self):
         """ Check that the export generate an attachment and email"""
-        params = json.loads(data_xls.get("data"))
-        mails = self.env["mail.mail"].search([])
-        attachments = self.env["ir.attachment"].search([])
+        params = json.loads(data_xls.get('data'))
+        mails = self.env['mail.mail'].search([])
+        attachments = self.env['ir.attachment'].search([])
         self.delay_export_obj.export(params)
-        new_mail = self.env["mail.mail"].search([]) - mails
-        new_attachment = self.env["ir.attachment"].search([]) - attachments
+        new_mail = self.env['mail.mail'].search([]) - mails
+        new_attachment = self.env['ir.attachment'].search([]) - attachments
         self.assertEqual(len(new_mail), 1)
-        self.assertEqual(new_attachment.datas_fname, "res.partner.xls")
+        self.assertEqual(new_attachment.datas_fname,
+                         "res.partner.xls")
 
     def test_cron_delete(self):
         """ Check that cron delete attachment after TTL"""
-        params = json.loads(data_csv.get("data"))
-        attachments = self.env["ir.attachment"].search([])
+        params = json.loads(data_csv.get('data'))
+        attachments = self.env['ir.attachment'].search([])
         self.delay_export_obj.export(params)
-        new_attachment = self.env["ir.attachment"].search([]) - attachments
-        time_to_live = (
-            self.env["ir.config_parameter"].sudo().get_param("attachment.ttl", 7)
-        )
-        date_today = fields.Date.today()
-        date_to_delete = date_today + relativedelta(days=-int(time_to_live))
-        # Update create_date with today - TTL
-        self.delay_export_obj.search([]).write({"create_date": date_to_delete})
-        self.delay_export_obj.sudo().cron_delete()
+        new_attachment = self.env['ir.attachment'].search([]) - attachments
+        time_to_live = self.env['ir.config_parameter'].sudo(). \
+            get_param('attachment.ttl', 7)
+        date_today = fields.Datetime.now()
+        date_past_ttl = date_today + relativedelta(days=int(time_to_live))
+        with freezegun.freeze_time(date_past_ttl):
+            self.delay_export_obj.cron_delete()
+
         # The attachment must be deleted
         self.assertFalse(new_attachment.exists())
