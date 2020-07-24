4fe0becf53f38fc3df71282917e3f1dec463a386
sbejaoui <souheil.bejaoui@acsone.eu>
2020-07-24 10:21:39 +0200

    [IMP] - Create queue_job_notify trigger at post_init_hook

--- queue_job/jobrunner/runner.py
+++ queue_job/jobrunner/runner.py
@@ -304,31 +304,6 @@ class Database(object):
 
     def _initialize(self):
         with closing(self.conn.cursor()) as cr:
-            # this is the trigger that sends notifications when jobs change
-            cr.execute(
-                """
-                DROP TRIGGER IF EXISTS queue_job_notify ON queue_job;
-
-                CREATE OR REPLACE
-                    FUNCTION queue_job_notify() RETURNS trigger AS $$
-                BEGIN
-                    IF TG_OP = 'DELETE' THEN
-                        IF OLD.state != 'done' THEN
-                            PERFORM pg_notify('queue_job', OLD.uuid);
-                        END IF;
-                    ELSE
-                        PERFORM pg_notify('queue_job', NEW.uuid);
-                    END IF;
-                    RETURN NULL;
-                END;
-                $$ LANGUAGE plpgsql;
-
-                CREATE TRIGGER queue_job_notify
-                    AFTER INSERT OR UPDATE OR DELETE
-                    ON queue_job
-                    FOR EACH ROW EXECUTE PROCEDURE queue_job_notify();
-            """
-            )
             cr.execute("LISTEN queue_job")
 
     @contextmanager
