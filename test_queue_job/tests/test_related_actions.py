44cca3dce5e4159659c3e1a2b18f6dc498fef6f4
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-10-30 17:59:33 +0100

    Deprecate job decorators
    * @job and @related_action are still supported but emit warnings and
      will be removed at some point (code to remove has a TODO with
      :job-no-decorator:)
    * the options previously set by the decorators are now stored on
      "queue.job.function" records, which can be explicitly created
      as XML records (and then editable!)
    * any method can be used with "with_delay()", if no "queue.job.function"
      exists, default values are used (root channel, default related
      action, ...)

--- test_queue_job/tests/test_related_actions.py
+++ test_queue_job/tests/test_related_actions.py
@@ -23,15 +23,9 @@ class TestRelatedAction(common.SavepointCase):
         self.assertEqual(act_kwargs, {"b": 4})
 
     def test_decorator_empty(self):
-        """ Job with decorator without value disable the default action
+        """Job with decorator without value disable the default action
 
-        The function is::
-
-            @job
-            @related_action()  # default action returns None
-            def testing_related_action__return_none(self):
-                return
-
+        The ``related_action`` configuration is: ``{"enable": False}``
         """
         # default action returns None
         job_ = self.record.with_delay().testing_related_action__return_none()
@@ -50,12 +44,7 @@ class TestRelatedAction(common.SavepointCase):
 
         When called on no record.
 
-        The function is::
-
-            @job
-            def testing_related_action__no(self):
-                return
-
+        The ``related_action`` configuration is: ``{}``
         """
         job_ = self.model.with_delay().testing_related_action__no()
         expected = None
@@ -75,12 +64,7 @@ class TestRelatedAction(common.SavepointCase):
 
         When called on one record.
 
-        The function is::
-
-            @job
-            def testing_related_action__no(self):
-                return
-
+        The ``related_action`` configuration is: ``{}``
         """
         job_ = self.record.with_delay().testing_related_action__no()
         expected = {
@@ -97,12 +81,7 @@ class TestRelatedAction(common.SavepointCase):
 
         When called on several record.
 
-        The function is::
-
-            @job
-            def testing_related_action__no(self):
-                return
-
+        The ``related_action`` configuration is: ``{}``
         """
         job_ = self.records.with_delay().testing_related_action__no()
         expected = {
@@ -119,12 +98,12 @@ class TestRelatedAction(common.SavepointCase):
 
         The function is::
 
-            @job
-            @related_action(action='testing_related__url',
-                            url='https://en.wikipedia.org/wiki/{subject}')
-            def testing_related_action__store(self):
-                return
+        The ``related_action`` configuration is::
 
+            {
+                "func_name": "testing_related__url",
+                "kwargs": {"url": "https://en.wikipedia.org/wiki/{subject}"}
+            }
         """
         job_ = self.record.with_delay().testing_related_action__store("Discworld")
         expected = {
