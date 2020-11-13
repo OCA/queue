bc11fbf340f2894f3fa134cdcbd7823f24642a41
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-11-13 13:39:07 +0100

    Store 'su' flag in records stored in JobSerialized fields
    Which means that the record the job is working on, or any record
    argument of the job keeps the 'su' flag.

--- test_queue_job/tests/test_job.py
+++ test_queue_job/tests/test_job.py
@@ -428,6 +428,26 @@ class TestJobs(JobCommonCase):
         self.assertEquals(([1],), job_instance.args)
         self.assertEquals({"mutable_kwarg": {"a": 1}}, job_instance.kwargs)
 
+    def test_store_env_su_no_sudo(self):
+        demo_user = self.env.ref("base.user_demo")
+        self.env = self.env(user=demo_user)
+        delayable = self.env["test.queue.job"].with_delay()
+        test_job = delayable.testing_method()
+        stored = test_job.db_record()
+        job_instance = Job.load(self.env, stored.uuid)
+        self.assertFalse(job_instance.recordset.env.su)
+        self.assertTrue(job_instance.user_id, demo_user)
+
+    def test_store_env_su_sudo(self):
+        demo_user = self.env.ref("base.user_demo")
+        self.env = self.env(user=demo_user)
+        delayable = self.env["test.queue.job"].sudo().with_delay()
+        test_job = delayable.testing_method()
+        stored = test_job.db_record()
+        job_instance = Job.load(self.env, stored.uuid)
+        self.assertTrue(job_instance.recordset.env.su)
+        self.assertTrue(job_instance.user_id, demo_user)
+
 
 class TestJobModel(JobCommonCase):
     def test_job_change_state(self):
