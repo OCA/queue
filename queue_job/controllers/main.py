deeb21b62d9e0ba382f6b6f7cfc56cd4fa56e279
Wolfgang Pichler <wpichler@callino.at>
2020-11-19 08:16:47 +0100

    Fix missing rollback on retried jobs
    When RetryableJobError was raised, any change done by the job was not
    rollbacked.
    
    Using `raise` would throw the exception up to the core and rollback, but
    we would have a stack trace in the logs for each try. Calling rollback
    manually (rollback also clears the env) hide the tracebacks, however,
    when the last try fails, the full traceback is still shown in the logs.
    
    Fixes #261

--- queue_job/controllers/main.py
+++ queue_job/controllers/main.py
@@ -89,9 +89,11 @@ class RunJobController(http.Controller):
                 if err.pgcode not in PG_CONCURRENCY_ERRORS_TO_RETRY:
                     raise
 
-                retry_postpone(job, tools.ustr(err.pgerror, errors='replace'),
-                               seconds=PG_RETRY)
                 _logger.debug('%s OperationalError, postponed', job)
+                raise RetryableJobError(
+                    tools.ustr(err.pgerror, errors='replace'),
+                    seconds=PG_RETRY
+                )
 
         except NothingToDoJob as err:
             if str(err):
@@ -106,6 +108,10 @@ class RunJobController(http.Controller):
             # delay the job later, requeue
             retry_postpone(job, str(err), seconds=err.seconds)
             _logger.debug('%s postponed', job)
+            # Do not trigger the error up because we don't want an exception
+            # traceback in the logs we should have the traceback when all
+            # retries are exhausted
+            env.cr.rollback()
 
         except (FailedJobError, Exception):
             buff = StringIO()
