c4a2b6718db137753c47329838e9333b48d0539d
Sébastien BEAU <sebastien.beau@akretion.com>
2020-11-20 15:39:01 +0100

    [IMP] : black, isort, prettier

--- test_queue_job/tests/test_job.py
+++ test_queue_job/tests/test_job.py
@@ -327,7 +327,7 @@ class TestJobs(JobCommonCase):
     """ Test jobs on other methods or with different job configuration """
 
     def test_description(self):
-        """ If no description is given to the job, it
+        """If no description is given to the job, it
         should be computed from the function
         """
         # if a docstring is defined for the function
@@ -610,8 +610,8 @@ class TestJobStorageMultiCompany(common.TransactionCase):
         )
 
     def test_job_no_company_id(self):
-        """ if we put an empty company_id in the context
-         jobs are created without company_id
+        """if we put an empty company_id in the context
+        jobs are created without company_id
         """
         env = self.env(context={"company_id": None})
         stored = self._create_job(env)
