65144fdaecf62f9a57b35308ceb9101bbb4671a4
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2021-02-01 10:32:56 +0100

    Remove initial create notification and follower
    Everytime a job is created, a mail.message "Queue Job created" is
    created. This is useless, as we already have the creation date and user,
    and nobody will ever want to receive a notification for a created job
    anyway.
    
    Removing the on creation auto-subscription of the user that created the
    job makes sense as well since we automatically subscribe the queue job
    managers for failures. Add the owner of the jobs to the followers on
    failures only as well.
    
    It allows to remove a lot of insertions of records (and of deletions
    when autovacuuming jobs).

--- queue_job/models/queue_job.py
+++ queue_job/models/queue_job.py
@@ -120,7 +120,10 @@ class QueueJob(models.Model):
             raise exceptions.AccessError(
                 _("Queue jobs must be created by calling 'with_delay()'.")
             )
-        return super().create(vals_list)
+        return super(
+            QueueJob,
+            self.with_context(mail_create_nolog=True, mail_create_nosubscribe=True),
+        ).create(vals_list)
 
     def write(self, vals):
         if self.env.context.get("_job_edit_sentinel") is not self.EDIT_SENTINEL:
@@ -192,9 +195,10 @@ class QueueJob(models.Model):
         # subscribe the users now to avoid to subscribe them
         # at every job creation
         domain = self._subscribe_users_domain()
-        users = self.env["res.users"].search(domain)
-        self.message_subscribe(partner_ids=users.mapped("partner_id").ids)
+        base_users = self.env["res.users"].search(domain)
         for record in self:
+            users = base_users | record.user_id
+            record.message_subscribe(partner_ids=users.mapped("partner_id").ids)
             msg = record._message_failed_job()
             if msg:
                 record.message_post(body=msg, subtype_xmlid="queue_job.mt_job_failed")
