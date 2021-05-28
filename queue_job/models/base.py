9db828c9313269b9c4b1acb3ddd07c512166dc9f
Guewen Baconnier <guewen.baconnier@qoqa.com>
2021-05-28 17:59:28 +0200

    Add powerful context manager for running tests on jobs
    * mock_jobs context manager with assert methods to check enqueued jobs
    * jobs are not stored in database, yet they can be inspected and performed
      during the test
    * the context manager works with graphs enqueued by delayable and with with_delay()
    * direct execution of jobs works with graphs by executing them synchronously
      following their topological sort

--- queue_job/models/base.py
+++ queue_job/models/base.py
@@ -3,7 +3,6 @@
 
 import functools
 import logging
-import os
 
 from odoo import api, models
 
@@ -55,21 +54,6 @@ class Base(models.AbstractModel):
         :return: instance of a DelayableRecordset
         :rtype: :class:`odoo.addons.queue_job.job.DelayableRecordset`
         """
-        # TODO Implement this for ``delayable``. 2 options:
-        # 1. store the jobs in db, traverse the graph and process
-        # them in the same transaction
-        # 2. do not store them, instead, traverse the graph and
-        # execute the calls directly.
-        if os.getenv('TEST_QUEUE_JOB_NO_DELAY'):
-            _logger.warn(
-                '`TEST_QUEUE_JOB_NO_DELAY` env var found. NO JOB scheduled.'
-            )
-            return self
-        if self.env.context.get('test_queue_job_no_delay'):
-            _logger.warn(
-                '`test_queue_job_no_delay` ctx key found. NO JOB scheduled.'
-            )
-            return self
         return DelayableRecordset(self, priority=priority,
                                   eta=eta,
                                   max_retries=max_retries,
