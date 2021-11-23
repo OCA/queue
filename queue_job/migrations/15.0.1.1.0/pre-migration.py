9543fea2d04c8b95ed7abea33943d4373cd5968c
Dũng (Trần Đình) <dungtd@trobz.com>
2021-11-23 19:37:47 +0700

    Forward migration scripts from #309 #328

--- queue_job/migrations/15.0.1.1.0/pre-migration.py
+++ queue_job/migrations/15.0.1.1.0/pre-migration.py
@@ -1,10 +1,6 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 
-import logging
-
 from odoo.tools.sql import column_exists
-
-_logger = logging.getLogger(__name__)
 
 
 def migrate(cr, version):
