7d58192ce2c63d2e451cb5e1c08bcf287680834c
Diep Huu Hoang <hoang@trobz.com>
2021-12-03 15:39:09 +0700

    [IMP] queue_job_batch, test_queue_job_batch: black, isort, prettier

--- queue_job_batch/__manifest__.py
+++ queue_job_batch/__manifest__.py
@@ -4,23 +4,23 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {
-    'name': 'Job Queue Batch',
-    'version': '12.0.1.0.1',
-    'author': 'Creu Blanca,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/queue',
-    'license': 'AGPL-3',
-    'category': 'Generic Modules',
-    'depends': [
-        'queue_job',
+    "name": "Job Queue Batch",
+    "version": "12.0.1.0.1",
+    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/queue",
+    "license": "AGPL-3",
+    "category": "Generic Modules",
+    "depends": [
+        "queue_job",
     ],
-    'qweb': [
-        'static/src/xml/systray.xml',
+    "qweb": [
+        "static/src/xml/systray.xml",
     ],
-    'data': [
-        'security/security.xml',
-        'security/ir.model.access.csv',
-        'views/queue_job_views.xml',
-        'views/queue_job_batch_views.xml',
-        'views/assets_backend.xml',
+    "data": [
+        "security/security.xml",
+        "security/ir.model.access.csv",
+        "views/queue_job_views.xml",
+        "views/queue_job_batch_views.xml",
+        "views/assets_backend.xml",
     ],
 }
