e8af807523bd40bab2e9e61755d126f2e5f25422
Diep Huu Hoang <hoang@trobz.com>
2021-12-03 16:58:11 +0700

    [14.0][MIG] queue_job_batch, test_queue_job_batch: Migration to 14.0

--- test_queue_job_batch/__manifest__.py
+++ test_queue_job_batch/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Test Job Queue Batch",
-    "version": "12.0.1.1.0",
+    "version": "14.0.1.0.0",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/queue",
     "license": "AGPL-3",
