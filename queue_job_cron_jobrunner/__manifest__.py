43b27cbcec07c278308698ef1a51983a253da812
Ivàn Todorovich <ivan.todorovich@gmail.com>
2022-03-02 16:08:03 -0300

    [ADD] queue_job_cron_jobrunner

---
+++ queue_job_cron_jobrunner/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Queue Job Cron Jobrunner",
+    "summary": "Run jobs without a dedicated JobRunner",
+    "version": "15.0.1.0.0",
+    "development_status": "Alpha",
+    "author": "Camptocamp SA, Odoo Community Association (OCA)",
+    "maintainers": ["ivantodorovich"],
+    "website": "https://github.com/OCA/queue",
+    "license": "AGPL-3",
+    "category": "Others",
+    "depends": ["queue_job"],
+    "data": [
+        "data/ir_cron.xml",
+        "views/ir_cron.xml",
+    ],
+}
