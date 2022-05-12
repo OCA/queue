f9f1157530b7fccacb6ebdefca27242df1a75da5
Guewen Baconnier <guewen.baconnier@qoqa.com>
2022-05-12 16:37:08 +0200

    Disable storing of context in jobs recordsets by default
    It can still be activated by overriding a method.

--- queue_job/readme/USAGE.rst
+++ queue_job/readme/USAGE.rst
@@ -131,10 +131,13 @@ Example:
 .. code-block:: python
 
    class Base(models.AbstractModel):
+
        _inherit = "base"
+
        @api.model
        def _job_prepare_context_before_enqueue_keys(self):
            """Keys to keep in context of stored jobs
+
            Empty by default for backward compatibility.
            """
            return ("tz", "lang", "allowed_company_ids", "force_company", "active_test")
--- test_queue_job/models/test_models.py
+++ test_queue_job/models/test_models.py
@@ -1,7 +1,7 @@
 # Copyright 2016 Camptocamp SA
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 from odoo.addons.queue_job.exception import RetryableJobError
 
@@ -32,6 +32,11 @@ class TestQueueJob(models.Model):
     _description = "Test model for queue.job"
 
     name = fields.Char()
+
+    # to test the context is serialized/deserialized properly
+    @api.model
+    def _job_prepare_context_before_enqueue_keys(self):
+        return ("tz", "lang")
 
     def testing_method(self, *args, **kwargs):
         """Method used for tests
