fb60c6fa8d511e839d238fcff7e166f9bd3d6451
Ashish Hirapara <ashish.hirapara1995@gmail.com>
2022-10-13 18:29:42 +0530

    [ADD][14.0] queue_job_context
    initial commit - Add context before queue

---
+++ queue_job_context/__manifest__.py
@@ -0,0 +1,15 @@
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
+# Copyright 2022 Ooops (ooops404.com)
+
+{
+    "name": "Queue Job Context",
+    "summary": "Queue Job, prepare context before enqueue keys",
+    "author": "Giovanni Serra - GSLab.it, Ooops, Odoo Community Association (OCA)",
+    "maintainers": ["AshishHirapara"],
+    "license": "LGPL-3",
+    "website": "https://github.com/OCA/queue",
+    "category": "Tools",
+    "version": "14.0.1.0.0",
+    "depends": ["queue_job"],
+    "installable": True,
+}
