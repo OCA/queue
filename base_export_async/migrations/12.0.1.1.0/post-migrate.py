9d37c8c4d2e5d446343f33363ee408518f5d6780
vincent-cowboy <vvr@cowboy.com>
2023-01-17 12:37:06 +0100

    [IMP] base_export_async: black, isort, prettier

--- base_export_async/migrations/12.0.1.1.0/post-migrate.py
+++ base_export_async/migrations/12.0.1.1.0/post-migrate.py
@@ -1,7 +1,7 @@
 # Copyright 2019 Camptocamp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, SUPERUSER_ID
+from odoo import SUPERUSER_ID, api
 from odoo.tools.sql import column_exists
 
 
