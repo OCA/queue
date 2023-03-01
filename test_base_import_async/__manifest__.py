50ec5b9d346ab40f413f411736fa35234475026f
Dũng (Trần Đình) <dungtd@trobz.com>
2023-03-01 18:43:58 +0700

    [16.0] base_import_async: Migration to 16.0

--- test_base_import_async/__manifest__.py
+++ test_base_import_async/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Test suite for base_import_async",
-    "version": "14.0.1.0.1",
+    "version": "16.0.1.0.0",
     "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/queue",
