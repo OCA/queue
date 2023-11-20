324c8b087575a2bb16a83f4cf9862c35d7263461
pcastelovigo <pablocv@gmx.es>
2023-11-20 14:10:48 +0100

    doctest

--- queue_job/jobrunner/runner.py
+++ queue_job/jobrunner/runner.py
@@ -400,24 +400,24 @@ class QueueJobRunner(object):
         return runner
 
     def get_db_names(self):
-       """
-       Get the database names based on certain conditions.
+        """
+        Get the database names based on certain conditions.
 
-       >>> os.environ["ODOO_QUEUE_JOB_JOBRUNNER_DB_NAME"] = "db1,db2"
-       >>> get_db_names()
-       ['db1', 'db2']
+        >>> os.environ["ODOO_QUEUE_JOB_JOBRUNNER_DB_NAME"] = "db1,db2"
+        >>> get_db_names()
+        ['db1', 'db2']
 
-       >>> os.environ.pop("ODOO_QUEUE_JOB_JOBRUNNER_DB_NAME", None)
-       >>> config["db_name"] = "db3,db4"
-       >>> get_db_names()
-       ['db3', 'db4']
+        >>> os.environ.pop("ODOO_QUEUE_JOB_JOBRUNNER_DB_NAME", None)
+        >>> config["db_name"] = "db3,db4"
+        >>> get_db_names()
+        ['db3', 'db4']
 
-       >>> os.environ.pop("ODOO_QUEUE_JOB_JOBRUNNER_DB_NAME", None)
-       >>> config["db_name"] = None
-       >>> queue_job_config = {"jobrunner_db_name": "db5,db6"}
-       >>> get_db_names()
-       ['db5', 'db6']
-       """
+        >>> os.environ.pop("ODOO_QUEUE_JOB_JOBRUNNER_DB_NAME", None)
+        >>> config["db_name"] = None
+        >>> queue_job_config = {"jobrunner_db_name": "db5,db6"}
+        >>> get_db_names()
+        ['db5', 'db6']
+        """
         if os.environ.get("ODOO_QUEUE_JOB_JOBRUNNER_DB_NAME"):
             db_names = os.environ["ODOO_QUEUE_JOB_JOBRUNNER_DB_NAME"].split(",")
         elif queue_job_config.get("jobrunner_db_name"):
