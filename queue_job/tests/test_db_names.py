02f55e012f3bd473b2c2a1b339a44043e9fca400
pcastelovigo <pablocv@gmx.es>
2023-11-20 10:53:24 +0100

    unit test for codecov

--- queue_job/tests/test_db_names.py
+++ queue_job/tests/test_db_names.py
@@ -7,11 +7,7 @@ class TestGetDbNames(TransactionCase):
     def test_get_db_names(self):
         os.environ["ODOO_QUEUE_JOB_JOBRUNNER_DB_NAME"] = "db1,db2"
         config["db_name"] = False
-        queue_job_config = {"jobrunner_db_name": "db3,db4"}
 
         db_names = self.env["queue.job"].get_db_names()
         self.assertEqual(db_names, ["db1", "db2"])
 
-        os.environ["ODOO_QUEUE_JOB_JOBRUNNER_DB_NAME"] = False
-        db_names = self.env["queue.job"].get_db_names()
-        self.assertEqual(db_names, ["db3", "db4"])
