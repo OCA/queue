b9722f93917d60a33d1eb0119198e44854c1f87c
Sander Lienaerts <sander.lienaerts@codeforward.nl>
2023-12-06 11:20:51 +0100

    [MIG] queue_job_cron_jobrunner: Migration to 17.0

--- queue_job_cron_jobrunner/__manifest__.py
+++ queue_job_cron_jobrunner/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Queue Job Cron Jobrunner",
     "summary": "Run jobs without a dedicated JobRunner",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "development_status": "Alpha",
     "author": "Camptocamp SA, Odoo Community Association (OCA)",
     "maintainers": ["ivantodorovich"],
