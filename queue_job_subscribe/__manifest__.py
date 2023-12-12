198e07b243e4a7edad506c100b7d6f320a33e8cc
David Ramia <david.ramia@processcontrol.es>
2023-12-12 07:40:53 +0100

    [MIG] queue_job_subscribe: Migration to 17.0

--- queue_job_subscribe/__manifest__.py
+++ queue_job_subscribe/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Queue Job Subscribe",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Acsone SA/NV, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/queue",
     "summary": "Control which users are subscribed to queue job notifications",
