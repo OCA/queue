460aec13deabed0f49ef46b4d822bb8c1bb6a249
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2024-03-18 12:17:08 +0530

    [IMP] queue_job_batch: Fix systray icon visibility for allowed group users only

--- queue_job_batch/static/src/js/systray_service.esm.js
+++ queue_job_batch/static/src/js/systray_service.esm.js
@@ -1,18 +1,24 @@
 /** @odoo-module **/
 
 import {QueueJobBatchMenuContainer} from "./batch_menu_container_view.esm";
-
 import {registry} from "@web/core/registry";
+import session from "web.session";
 
 const systrayRegistry = registry.category("systray");
 
 export const systrayService = {
     start() {
-        systrayRegistry.add(
-            "queue_job_batch.QueueJobBatchMenu",
-            {Component: QueueJobBatchMenuContainer},
-            {sequence: 99}
-        );
+        session
+            .user_has_group("queue_job_batch.group_queue_job_batch_user")
+            .then(function (has_group) {
+                if (has_group) {
+                    systrayRegistry.add(
+                        "queue_job_batch.QueueJobBatchMenu",
+                        {Component: QueueJobBatchMenuContainer},
+                        {sequence: 99}
+                    );
+                }
+            });
     },
 };
 
