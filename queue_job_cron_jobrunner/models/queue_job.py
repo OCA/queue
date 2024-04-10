ab1425135f0e30f98dff53952b0ea7ea934673c4
Pierre Verkest <pierreverkest84@gmail.com>
2024-04-10 18:29:50 +0200

    [FIX] queue_job_cron_jobrunner: use priority to select job
    * use FIFO, firt createad job will be treat first
    * if priority are different it take the precedent
    
    Yet we are not using channel priority into account

--- queue_job_cron_jobrunner/models/queue_job.py
+++ queue_job_cron_jobrunner/models/queue_job.py
@@ -40,7 +40,7 @@ class QueueJob(models.Model):
             FROM queue_job
             WHERE state = 'pending'
             AND (eta IS NULL OR eta <= (now() AT TIME ZONE 'UTC'))
-            ORDER BY date_created DESC
+            ORDER BY priority, date_created
             LIMIT 1 FOR NO KEY UPDATE SKIP LOCKED
             """
         )
