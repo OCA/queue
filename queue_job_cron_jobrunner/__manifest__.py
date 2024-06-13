9142be5620edfc1fdaca2b5df73d9e7a1c005ed3
Vincent Van Rossem <vincent.vanrossem@camptocamp.com>
2024-06-13 17:31:28 +0200

    Replace 'Camptocamp SA' with 'Camptocamp'

--- queue_job_cron_jobrunner/__manifest__.py
+++ queue_job_cron_jobrunner/__manifest__.py
@@ -3,7 +3,7 @@
     "summary": "Run jobs without a dedicated JobRunner",
     "version": "15.0.2.0.0",
     "development_status": "Alpha",
-    "author": "Camptocamp SA, Odoo Community Association (OCA)",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
     "maintainers": ["ivantodorovich"],
     "website": "https://github.com/OCA/queue",
     "license": "AGPL-3",
