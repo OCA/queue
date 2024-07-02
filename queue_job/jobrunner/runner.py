1b3fd3a7e0a109267a154c583c204bf2f78db792
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2024-07-02 13:50:11 +0200

    [FIX] queue_job: close connection to databases without job queue
    Without this, we leak connections to Databases that don't have queue_job
    installed.

--- queue_job/jobrunner/runner.py
+++ queue_job/jobrunner/runner.py
@@ -423,6 +423,8 @@ class QueueJobRunner:
                     for job_data in cr:
                         self.channel_manager.notify(db_name, *job_data)
                 _logger.info("queue job runner ready for db %s", db_name)
+            else:
+                db.close()
 
     def run_jobs(self):
         now = _odoo_now()
