a57dcb7344757baa03b963f027f3df357fe0d7fd
Benoit Aimont <benoit.aimont@acsone.eu>
2025-06-26 10:22:55 +0200

    Make queue_job_cron LGPL

--- queue_job_cron/__manifest__.py
+++ queue_job_cron/__manifest__.py
@@ -1,12 +1,12 @@
 # Copyright 2019 ACSONE SA/NV (<http://acsone.eu>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 
 {
     "name": "Scheduled Actions as Queue Jobs",
     "version": "18.0.1.1.1",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/queue",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "category": "Generic Modules",
     "depends": ["queue_job"],
     "data": ["data/data.xml", "views/ir_cron_view.xml"],
