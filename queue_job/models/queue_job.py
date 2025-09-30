3cdbc4af252199d549620d5b774de443d3ec7ea4
Milan Topuzov <milan.topuzov@salewise.io>
2025-09-30 02:07:47 +0200

    [19.0] queue_job: replace self._cr and raw DDL\n\n- Use self.env.cr (self._cr deprecated in 19.0)\n- Use odoo.tools.sql.create_index with index_exists for idempotent index creation\n- Inline comment noting Odoo 19 change

--- queue_job/models/queue_job.py
+++ queue_job/models/queue_job.py
@@ -6,7 +6,8 @@ import random
 from datetime import datetime, timedelta
 
 from odoo import _, api, exceptions, fields, models
-from odoo.tools import config, html_escape, index_exists
+from odoo.tools import config, html_escape
+from odoo.tools.sql import create_index, index_exists
 
 from odoo.addons.base_sparse_field.models.fields import Serialized
 
@@ -127,20 +128,28 @@ class QueueJob(models.Model):
     worker_pid = fields.Integer(readonly=True)
 
     def init(self):
+        # Odoo 19: self._cr deprecated, use self.env.cr; prefer tools.sql helpers for idempotent DDL
+        cr = self.env.cr
         index_1 = "queue_job_identity_key_state_partial_index"
         index_2 = "queue_job_channel_date_done_date_created_index"
-        if not index_exists(self._cr, index_1):
+        if not index_exists(cr, index_1):
             # Used by Job.job_record_with_same_identity_key
-            self._cr.execute(
-                "CREATE INDEX queue_job_identity_key_state_partial_index "
-                "ON queue_job (identity_key) WHERE state in ('pending', "
-                "'enqueued', 'wait_dependencies') AND identity_key IS NOT NULL;"
+            create_index(
+                cr,
+                index_1,
+                "queue_job",
+                ["identity_key"],
+                where="state in ('pending','enqueued','wait_dependencies') AND identity_key IS NOT NULL",
+                comment="Queue Job: partial index for identity_key on active states",
             )
-        if not index_exists(self._cr, index_2):
+        if not index_exists(cr, index_2):
             # Used by <queue.job>.autovacuum
-            self._cr.execute(
-                "CREATE INDEX queue_job_channel_date_done_date_created_index "
-                "ON queue_job (channel, date_done, date_created);"
+            create_index(
+                cr,
+                index_2,
+                "queue_job",
+                ["channel", "date_done", "date_created"],
+                comment="Queue Job: index to accelerate autovacuum",
             )
 
     @api.depends("dependencies")
