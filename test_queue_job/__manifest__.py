9e9ad380d923de80e10263e7f81f42059fa73957
Richard deMeester <richard.demeester@willdooit.com>
2025-10-01 08:54:56 +0200

    Update version numbers for ported modules

--- test_queue_job/__manifest__.py
+++ test_queue_job/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Queue Job Tests",
-    "version": "18.0.2.0.0",
+    "version": "19.0.1.0.0",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "category": "Generic Modules",
@@ -15,5 +15,5 @@
         "security/ir.model.access.csv",
         "data/queue_job_test_job.xml",
     ],
-    "installable": False,
+    "installable": True,
 }
